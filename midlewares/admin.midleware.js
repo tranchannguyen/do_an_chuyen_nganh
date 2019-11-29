0b1635966271d6f630aa8ab6f7c53112e06a233e
Nguyen Tran Chan <kuchan132@gmail.com>
2019-11-29 21:51:54 +0700

    fix_admin_midleware,add_index_webpage

--- midlewares/admin.midleware.js
+++ midlewares/admin.midleware.js
@@ -1,11 +1,9 @@
 var User = require('../models/user.model');
-module.exports.requireAdmin = async function(req,res){
+module.exports.requireAdmin = async function(req,res,next){
     var userlogin = await User.find({email: req.signedCookies.userEmail})
 	var authAdmin = userlogin[0];
-	if(authAdmin.admin == true){
-		var users = await User.find();
-		res.render('users/index',{
-		 users: users
-	 });
-	}else res.redirect('/products')
+	if(!authAdmin.admin == true){
+		 return res.redirect('/products')
+	 }
+	next();
 }
